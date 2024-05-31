f1a6bc2b660da8147191340b55c559da5f386a90
Henry18C <henryisrael45@gmail.com>
2024-05-30 19:47:38 -0500

    Taller  14: Directivas vue

--- js/app.js
+++ js/app.js
@@ -1,51 +1,68 @@
-
-const estudiantes= [{nombre: "Henry", apellido: "Coyago"},
-{nombre: "Pepe", apellido: "Jimenez"},
-{nombre: "Pedro", apellido: "Lopez"},
-{nombre: "Pablo", apellido: "Mendez"},
-{nombre: "Jose", apellido: "Torres"},
+console.log(Vue);
+ 
+const estudiantes = [{ nombre: "Edison", apellido: "Cayambe" },
+{ nombre: "Daniel", apellido: "Mora" },
+{ nombre: "Carlos", apellido: "Teran" },
+{ nombre: "Romeo", apellido: "Castillo" },
+{ nombre: "Andrea", apellido: "Paez" },
 ];
-
-
-
+ 
 const app = Vue.createApp({
-  //  template: `
-
-   // <h1>Hola mundo desde Vue.JS</h1>
-    //<p> {{1+1}} </p>
-   // <p> {{[1,2,3,4,5]}} </p>
-    //<p> {{true?'True': 'False'}} </p>
-    
-  //  `
-
-  data(){
-    return  {
-        mensaje: "Hola Mundo Propieddad Reactiva",
-        valor: 7,
-        lista: estudiantes
-    }
-  }, 
-
-  methods:{
-    cambiarTexto(){
-        this.mensaje= "Texto cambiado";
-        
-
-    },
-
-    sumar(){
-        this.valor= this.valor+100;
-    },
-
-    agregar(){
-        const nuevo={
-            nombre: "Marlon",
-            apellido: "Vela"
+    /*template: `
+    <h1>Hola mundo desde Vue.JS</h1>
+    <p>
+    {{1+1}}</p>
+    <p>{{[1,2,3,4,5]}}</p>
+    <p>{{false?'True':'False'}}</p>
+    `*/
+    data() {
+        return {
+            //puedo declarar cualquier tipo de dato, los debo separa por comas
+            mensaje: "Hola mundo propiedad reactiva",
+            valor: 7,
+            lista: estudiantes,
+            nombre: null,
+            apellido:null
         }
-        this.lista.unshift(nuevo);
+    },
+    //Destinado para los métodos
+    //Cuando quiera creear una funcionalidad debe estar en los methods
+    methods: {
+        cambiarTexto() {
+            //Toda propiedad reactiva que se usa dentro de codificacion js debe tener la palabra reservada this en especial archivos js
+            this.mensaje = "Texto cambiado"
+ 
+        },
+        sumar() {
+            this.valor = this.valor + 100;
+        },
+        agregar(){
+            console.log(this.nombre);
+            console.log(this.apellido);
+            const nuevo={
+                nombre:this.nombre,
+                apellido:this.apellido
+            }
+            this.lista.unshift(nuevo);
+            this.resetear();
+        },
+        resetear(){
+            this.nombre=null;
+            this.apellido=null;
+        },
+        agregar2({charCode}){
+
+            console.log(charCode);
+            if(charCode !==13) return;
+            if(this.nombre!==null && this.apellido!==null){
+                this.agregar();
+            }
+ 
+            
+        }
+ 
+ 
     }
-  }
-
 });
-
+ 
 app.mount('#myApp'); //configuro mi aplicación
